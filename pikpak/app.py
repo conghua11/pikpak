42f0f1108bc0ee5e5ef8e24689fab39bbdfdc48f
atong <100357522+conghua11@users.noreply.github.com>
2024-06-29 20:47:26 +0800

    Update app.py

--- pikpak/app.py
+++ pikpak/app.py
@@ -21,4 +21,6 @@ def pass_verify(xid, pid, traceid, result):
     return 42
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+   
